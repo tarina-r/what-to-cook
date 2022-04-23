ff700cff8bbcbc48f7fe2b89dcf55da68ad70d34
tarina <tarina428@gmail.com>
2022-04-23 20:50:27 +0600

    json to object conversion

---
+++ lib/model/recipe.model.dart
@@ -0,0 +1,44 @@
+class Recipe {
+  List<Items>? items;
+
+  Recipe({this.items});
+
+  Recipe.fromJson(Map<String, dynamic> json) {
+    if (json['items'] != null) {
+      items = <Items>[];
+      json['items'].forEach((v) {
+        items!.add(new Items.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.items != null) {
+      data['items'] = this.items!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Items {
+  late String id;
+  late String name;
+  late String description;
+
+  Items({required this.id, required this.name, required this.description});
+
+  Items.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    description = json['description'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['description'] = this.description;
+    return data;
+  }
+}
--- lib/screen/recipe_list_screen.dart
+++ lib/screen/recipe_list_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
+import 'package:cook/model/recipe.model.dart';
 import 'package:cook/screen/recipe_details_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class RecipeListScreen extends StatefulWidget {
   const RecipeListScreen({Key? key}) : super(key: key);
@@ -9,16 +13,62 @@ class RecipeListScreen extends StatefulWidget {
 }
 
 class _RecipeListScreenState extends State<RecipeListScreen> {
-  navigate(BuildContext context){
+  Recipe _recipe = new Recipe();
+
+  @override
+  initState() {
+    readJson();
+    super.initState();
+  }
+
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/sample.json');
+    final data = await json.decode(response);
+    setState(() {
+      // items = data["items"];
+      // _recipe = data;
+      _recipe = Recipe.fromJson(data);
+    });
+  }
+
+  navigate(BuildContext context) {
     Navigator.push(context, MaterialPageRoute(builder: (context) => const RecipeListDetailsScreen()));
   }
 
   @override
   Widget build(BuildContext context) {
+    List<Items> items = _recipe.items ?? [];
+
     return Scaffold(
       appBar: AppBar(title: const Text("Recipe List")),
       body: Container(
-        child: ElevatedButton(onPressed: () => navigate(context),child: const Text("Details"),),
+        child: Column(
+          children: [
+            //ElevatedButton(
+            //onPressed: () => navigate(context),
+            //child: const Text("Details"),
+            //),
+            //ElevatedButton(onPressed: readJson, child: const Text('Load Data')),
+            items.isNotEmpty
+                ? Expanded(
+                    child: ListView.builder(
+                      itemCount: items.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: const EdgeInsets.all(10),
+                          child: ListTile(
+                            leading: Text(items[index].id),
+                            title: Text(items[index].name),
+                            subtitle: Text(items[index].description),
+                            onTap: () => navigate(context),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                : Container()
+          ],
+        ),
       ),
     );
   }
